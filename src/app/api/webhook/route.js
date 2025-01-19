057e5e7ccb013ac79ca98691006c083bffe54ef8
Matheswaran <138348719+MathesMaddy@users.noreply.github.com>
2025-01-19 17:49:47 +0530

    Update route.js

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -4,19 +4,32 @@ import {buffer} from 'micro';
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 export async function POST(request) {
+    
     await initMongoose(); 
 
-    const SigningSecret = whsec_7goP1v41DoxEBaNlcMW5zA45LgmPVeET;
-    const payload = await buffer(request.body);
-    const signature = request.headers['stripe-signature'];
-    const event = stripe.webhooks.constructEvent(payload, signature, SigningSecret);
+    const SigningSecret = process.env.STRIPE_WEBHOOK_SECRET;
+    const payload = await request.text(); 
+    const signature = await request.headers.get('stripe-signature');
 
-    if(event?.type === 'checkout.session.completed') {
-        
-        const metadata = event.data?.object?.metadata;
-        const paymentStatus = event.data?.object?.payment_status;
-        if(metadata?.OrderId && paymentStatus === 'paid') {
-            await Order.findByIdAndUpdate(metadata.OrderId, {paid : 1})
-        }
+    const event = await stripe.webhooks.constructEvent(payload, signature, SigningSecret);
+    
+    console.log(event);
+    if(event?.type === 'checkout.session.completed') {        
+        const metadata = await event.data?.object?.metadata;
+        console.log(metadata);
+        const paymentStatus = await event.data?.object?.payment_status;
+        console.log(paymentStatus);
+        if(metadata?.orderId && paymentStatus === 'paid') {
+            await Order.findOneAndUpdate({ _id: metadata.orderId }, { $set : {paid : 1}}, {new : true})
+            console.log('ok update');
+        }        
     }
+    return new Response('ok')
+    
 }
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
